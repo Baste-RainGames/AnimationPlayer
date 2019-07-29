e6144c9d7107ce3502dbca0087ce6128729cf8fc
Baste Nesse Buanes <baste@rain-games.com>
2019-07-29 13:35:59 +0200

    Major reorganization of the project. Instead of being standard Unity project where you copy over the files you want, this is now a package, meant to be used with the  Unity package manager. This commit breaks all old GUIDS, sorry about that!

--- Assets/Scripts/AnimationPlayer/Editor/SerializedPropertyExtension/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("AnimationPlayer")]
-[assembly: InternalsVisibleTo("AnimationPlayerEditor")]
-[assembly: InternalsVisibleTo("AnimationPlayerUnitTests")]

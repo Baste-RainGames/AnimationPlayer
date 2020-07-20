daaf6d66f0079af8dcd9d40419ad88b74881ab8a
Baste Nesse Buanes <baste@rain-games.com>
2020-07-20 16:31:25 +0200

    progress

--- Editor/NewNewAnimationPlayerEditor.cs
+++ Editor/NewNewAnimationPlayerEditor.cs
@@ -7,7 +7,6 @@ using UnityEngine.UIElements;
 
 namespace Animation_Player
 {
-
 [CustomEditor(typeof(AnimationPlayer))]
 public class NewNewAnimationPlayerEditor : Editor
 {
@@ -22,7 +21,7 @@ public class NewNewAnimationPlayerEditor : Editor
         public VisualElement visualElement;
         public SerializedObject serializedObject;
 
-        public AnimationPlayerUINode(UIRoot root, SerializedObject serializedObject, AnimationPlayerUINode parent)
+        public AnimationPlayerUINode(UIRoot root, SerializedObject serializedObject)
         {
             this.root = root;
             this.serializedObject = serializedObject;
@@ -35,12 +34,12 @@ public class NewNewAnimationPlayerEditor : Editor
         public LayersContainer layersContainer;
         public int selectedLayer;
 
-        public UIRoot(SerializedObject serializedObject) : base(null, serializedObject, null)
+        public UIRoot(SerializedObject serializedObject) : base(null, serializedObject)
         {
             root = this;
             visualElement = new VisualElement();
-            topBar = new TopBar(this, serializedObject, this);
-            layersContainer = new LayersContainer(this, serializedObject, this);
+            topBar = new TopBar(this, serializedObject);
+            layersContainer = new LayersContainer(this, serializedObject);
 
             visualElement.Add(topBar.visualElement);
             visualElement.Add(layersContainer.visualElement);
@@ -58,15 +57,15 @@ public class NewNewAnimationPlayerEditor : Editor
         public AddLayerButton addLayerButton;
         public RemoveLayerButton removeLayerButton;
 
-        public TopBar(UIRoot root, SerializedObject serializedObject, AnimationPlayerUINode parent) : base(root, serializedObject, parent)
+        public TopBar(UIRoot root, SerializedObject serializedObject) : base(root, serializedObject)
         {
             visualElement = new VisualElement();
             visualElement.styleSheets.Add(AssetDatabase.LoadAssetAtPath<StyleSheet>("Packages/com.baste.animationplayer/Editor/UXML/AnimationPlayer.uss"));
             visualElement.AddToClassList("topBar");
 
-            layerDropdown     = new LayerDropdown    (root, serializedObject, parent);
-            addLayerButton    = new AddLayerButton   (root, serializedObject, parent);
-            removeLayerButton = new RemoveLayerButton(root, serializedObject, parent);
+            layerDropdown     = new LayerDropdown    (root, serializedObject);
+            addLayerButton    = new AddLayerButton   (root, serializedObject);
+            removeLayerButton = new RemoveLayerButton(root, serializedObject);
 
             visualElement.Add(layerDropdown    .visualElement);
             visualElement.Add(addLayerButton   .visualElement);
@@ -78,7 +77,7 @@ public class NewNewAnimationPlayerEditor : Editor
     {
         private PopupField<Layer> popupField;
 
-        public LayerDropdown(UIRoot root, SerializedObject serializedObject, AnimationPlayerUINode parent) : base(root, serializedObject, parent)
+        public LayerDropdown(UIRoot root, SerializedObject serializedObject) : base(root, serializedObject)
         {
             visualElement = new VisualElement();
         }
@@ -114,7 +113,7 @@ public class NewNewAnimationPlayerEditor : Editor
 
     public class RemoveLayerButton : AnimationPlayerUINode
     {
-        public RemoveLayerButton(UIRoot root, SerializedObject serializedObject, AnimationPlayerUINode parent) : base(root, serializedObject, parent)
+        public RemoveLayerButton(UIRoot root, SerializedObject serializedObject) : base(root, serializedObject)
         {
             visualElement = new Button
             {
@@ -133,7 +132,7 @@ public class NewNewAnimationPlayerEditor : Editor
 
     public class AddLayerButton : AnimationPlayerUINode
     {
-        public AddLayerButton(UIRoot root, SerializedObject serializedObject, AnimationPlayerUINode parent) : base(root, serializedObject, parent)
+        public AddLayerButton(UIRoot root, SerializedObject serializedObject) : base(root, serializedObject)
         {
             visualElement = new Button
             {
@@ -155,7 +154,7 @@ public class NewNewAnimationPlayerEditor : Editor
         public List<Layer> layers;
         private SerializedProperty layersProp;
 
-        public LayersContainer(UIRoot root, SerializedObject serializedObject, AnimationPlayerUINode parent) : base(root, serializedObject, parent)
+        public LayersContainer(UIRoot root, SerializedObject serializedObject) : base(root, serializedObject)
         {
             visualElement = new VisualElement();
             layers = new List<Layer>();
@@ -164,15 +163,15 @@ public class NewNewAnimationPlayerEditor : Editor
 
             for (int i = 0; i < layersProp.arraySize; i++)
             {
-                CreateLayer(root, serializedObject, this, i);
+                CreateLayer(root, serializedObject, i);
             }
         }
 
         public int NumLayers => layersProp.arraySize;
 
-        private void CreateLayer(UIRoot root, SerializedObject serializedObject, AnimationPlayerUINode parent, int i)
+        private void CreateLayer(UIRoot root, SerializedObject serializedObject, int layerIndex)
         {
-            var layer = new Layer(root, serializedObject, parent, i);
+            var layer = new Layer(root, serializedObject, layerIndex);
             layers.Add(layer);
             visualElement.Add(layer.visualElement);
         }
@@ -180,13 +179,13 @@ public class NewNewAnimationPlayerEditor : Editor
         public void SetLayerVisible(int index)
         {
             for (int i = 0; i < layers.Count; i++)
-                layers[i].visualElement.visible = index == i;
+                layers[i].visualElement.style.display = index == i ? DisplayStyle.Flex : DisplayStyle.None;
         }
 
         public void AddNewLayer()
         {
             layersProp.arraySize++;
-            CreateLayer(root, serializedObject, this, layersProp.arraySize - 1);
+            CreateLayer(root, serializedObject, layersProp.arraySize - 1);
         }
 
         public void RemoveLayer(int layer)
@@ -205,21 +204,24 @@ public class NewNewAnimationPlayerEditor : Editor
         public SerializedProperty serializedProperty;
 
         public EditLayerSection editLayerSection;
-        public EditStatesSection editStateSection;
+        public AddAndSearchStatesSection addAndSearchStatesSection;
+        public EditStatesSection editStatesSection;
 
-        public Layer(UIRoot root, SerializedObject serializedObject, AnimationPlayerUINode parent, int index) : base(root, serializedObject, parent)
+        public Layer(UIRoot root, SerializedObject serializedObject, int index) : base(root, serializedObject)
         {
             visualElement = new VisualElement();
             visualElement.AddToClassList("animationLayer");
 
             this.index = index;
-            serializedProperty = serializedObject.FindProperty("layers").GetArrayElementAtIndex(index);
+            serializedProperty = serializedObject.FindProperty(nameof(AnimationPlayer.layers)).GetArrayElementAtIndex(index);
 
-            editLayerSection = new EditLayerSection(root, serializedObject, parent);
-            editStateSection = new EditStatesSection(root, serializedObject, parent);
+            editLayerSection = new EditLayerSection(root, serializedObject, serializedProperty);
+            addAndSearchStatesSection = new AddAndSearchStatesSection(root, serializedObject);
+            editStatesSection = new EditStatesSection(root, serializedObject);
 
-            visualElement.Add(editLayerSection.visualElement);
-            visualElement.Add(editStateSection.visualElement);
+            visualElement.Add(editLayerSection         .visualElement);
+            visualElement.Add(addAndSearchStatesSection.visualElement);
+            visualElement.Add(editStatesSection        .visualElement);
         }
 
         public static string GetNameOf(Layer layer)
@@ -238,36 +240,234 @@ public class NewNewAnimationPlayerEditor : Editor
                 return;
 
             index = newIndex;
-            serializedProperty = serializedObject.FindProperty("layers").GetArrayElementAtIndex(newIndex);
-            editLayerSection.ParentIndexChanged(newIndex);
-            editStateSection.ParentIndexChanged(newIndex);
+            serializedProperty = serializedObject.FindProperty(nameof(AnimationPlayer.layers)).GetArrayElementAtIndex(newIndex);
+            editLayerSection.ParentSerializedPropertyChanged(serializedProperty);
+            editStatesSection.ParentIndexChanged(newIndex);
         }
     }
 
     public class EditLayerSection : AnimationPlayerUINode
     {
-        public EditLayerSection(UIRoot root, SerializedObject serializedObject, AnimationPlayerUINode parent) : base(root, serializedObject, parent)
+        public LayerName      layerName;
+        public LayerType      layerType;
+        public LayerStartWeight    LayerStartWeight;
+        public AvatarMaskNode avatarMask;
+
+        public EditLayerSection(UIRoot root, SerializedObject serializedObject, SerializedProperty layerProp) : base(
+            root, serializedObject)
         {
             visualElement = new VisualElement();
             visualElement.AddToClassList("animationLayer__subSection");
 
+            layerName   = new LayerName(root, serializedObject, layerProp);
+            layerType   = new LayerType(root, serializedObject, layerProp);
+            LayerStartWeight = new LayerStartWeight(root, serializedObject, layerProp);
+            avatarMask  = new AvatarMaskNode(root, serializedObject, layerProp);
 
+            visualElement.Add(layerName.visualElement);
+            visualElement.Add(layerType.visualElement);
+            visualElement.Add(LayerStartWeight.visualElement);
+            visualElement.Add(avatarMask.visualElement);
         }
 
-        public void ParentIndexChanged(int newIndex)
+        public void ParentSerializedPropertyChanged(SerializedProperty newLayersProp)
         {
+            layerName.Rebind(newLayersProp);
+            layerType.Rebind(newLayersProp);
+            LayerStartWeight.Rebind(newLayersProp);
+            avatarMask.Rebind(newLayersProp);
+        }
+    }
 
+    public class LayerName : AnimationPlayerUINode
+    {
+        private TextField textField;
+
+        public LayerName(UIRoot root, SerializedObject serializedObject, SerializedProperty layerProp) :
+            base(root, serializedObject)
+        {
+            visualElement = textField = new TextField("Layer Name");
+            Rebind(layerProp);
+        }
+
+        public void Rebind(SerializedProperty newLayerProp)
+        {
+            textField.BindProperty(newLayerProp.FindPropertyRelative(nameof(AnimationLayer.name)));
+        }
+    }
+
+    public class LayerType : AnimationPlayerUINode
+    {
+        private EnumField enumField;
+
+        public LayerType(UIRoot root, SerializedObject serializedObject, SerializedProperty layerProp) :
+            base(root, serializedObject)
+        {
+            visualElement = enumField = new EnumField("Layer Type");
+            Rebind(layerProp);
+        }
+
+        public void Rebind(SerializedProperty newLayerProp)
+        {
+            enumField.BindProperty(newLayerProp.FindPropertyRelative(nameof(AnimationLayer.type)));
+        }
+    }
+
+    public class LayerStartWeight : AnimationPlayerUINode
+    {
+        private Slider slider;
+
+        public LayerStartWeight(UIRoot root, SerializedObject serializedObject, SerializedProperty layerProp) :
+            base(root, serializedObject)
+        {
+            visualElement = slider = new Slider("Layer Start Weight", 0f, 1f);
+            Rebind(layerProp);
+        }
+
+        public void Rebind(SerializedProperty newLayersProp)
+        {
+            slider.BindProperty(newLayersProp.FindPropertyRelative(nameof(AnimationLayer.startWeight)));
+        }
+    }
+
+    public class AvatarMaskNode : AnimationPlayerUINode
+    {
+        private ObjectField objectField;
+
+        public AvatarMaskNode(UIRoot root, SerializedObject serializedObject, SerializedProperty layerProp) : base(root, serializedObject)
+        {
+            visualElement = objectField = new ObjectField("Avatar Mask");
+            objectField.objectType = typeof(AvatarMask);
+            Rebind(layerProp);
+        }
+
+        public void Rebind(SerializedProperty newLayersProp)
+        {
+            objectField.BindProperty(newLayersProp.FindPropertyRelative(nameof(AnimationLayer.mask)));
+        }
+    }
+
+    public class AddAndSearchStatesSection : AnimationPlayerUINode
+    {
+        public AddStateButton addStateButton;
+
+        public AddAndSearchStatesSection(UIRoot root, SerializedObject serializedObject) : base(root, serializedObject)
+        {
+            visualElement = new VisualElement();
+            visualElement.AddToClassList("animationLayer__subSection");
+            visualElement.AddToClassList("animationLayer__addAndSearchStatesSection");
+
+            addStateButton = new AddStateButton(root, serializedObject);
+
+            visualElement.Add(addStateButton.visualElement);
+        }
+    }
+
+    public class AddStateButton : AnimationPlayerUINode
+    {
+        private static List<Type> allStateTypes = new List<Type>
+        {
+            typeof(SingleClip),
+            typeof(BlendTree1D),
+            typeof(BlendTree2D),
+            typeof(Sequence),
+            typeof(PlayRandomClip)
+        };
+
+        public AddStateButton(UIRoot root, SerializedObject serializedObject) : base(root, serializedObject)
+        {
+            Button button;
+            visualElement = button = new Button();
+
+            button.clickable = new Clickable(AddStateClicked);
+            button.text = "Add State";
+        }
+
+        private void AddStateClicked()
+        {
+            var gm = new GenericMenu();
+            foreach (var stateType in allStateTypes)
+                gm.AddItem(new GUIContent(stateType.Name), false, () => StateToAddSelected(stateType));
+            gm.ShowAsContext();
+        }
+
+        private void StateToAddSelected(Type stateType)
+        {
+            var layerProp = root.layersContainer.layers[root.selectedLayer].serializedProperty;
+
+            SerializedProperty stateProp;
+            var serializedOrder = AppendArray(layerProp, "serializedStateOrder");
+
+            if (stateType == typeof(SingleClip))
+            {
+                stateProp = AppendArray(layerProp, "serializedSingleClipStates");
+            }
+            else if (stateType == typeof(BlendTree1D))
+            {
+                stateProp = AppendArray(layerProp, "serializedBlendTree1Ds");
+                stateProp.FindPropertyRelative("blendVariable").stringValue = "blend";
+                stateProp.FindPropertyRelative("compensateForDifferentDurations").boolValue = true;
+            }
+            else if (stateType == typeof(BlendTree2D))
+            {
+                stateProp = AppendArray(layerProp, "serializedBlendTree2Ds");
+                stateProp.FindPropertyRelative("blendVariable").stringValue = "blend";
+                stateProp.FindPropertyRelative("blendVariable2").stringValue = "blend2";
+                stateProp.FindPropertyRelative("compensateForDifferentDurations").boolValue = true;
+            }
+            else if (stateType == typeof(PlayRandomClip))
+            {
+                stateProp = AppendArray(layerProp, "serializedSelectRandomStates");
+            }
+            else if (stateType == typeof(Sequence))
+            {
+                stateProp = AppendArray(layerProp, "serializedSequences");
+            }
+            else
+            {
+                Debug.LogError($"Adding state of type {stateType.Name} not implemented!");
+                return;
+            }
+
+            HandleSharedProps();
+            HandleGUIDOfNewState();
+
+            layerProp.serializedObject.ApplyModifiedProperties();
+
+            root.layersContainer.layers[root.selectedLayer].editStatesSection.OnStateAdded();
+
+            SerializedProperty AppendArray(SerializedProperty prop, string arrayPropName)
+            {
+                var arrayProp = prop.FindPropertyRelative(arrayPropName);
+                arrayProp.arraySize++;
+                return arrayProp.GetArrayElementAtIndex(arrayProp.arraySize - 1);
+            }
+
+            void HandleSharedProps()
+            {
+                stateProp.FindPropertyRelative("speed").doubleValue = 1d;
+                stateProp.FindPropertyRelative("name").stringValue = $"New {ObjectNames.NicifyVariableName(stateType.Name)}";
+            }
+
+            void HandleGUIDOfNewState()
+            {
+                var guid = SerializedGUID.Create().GUID.ToString();
+                stateProp.FindPropertyRelative("guid").FindPropertyRelative("guidSerialized").stringValue = guid;
+                serializedOrder.FindPropertyRelative("guidSerialized").stringValue = guid;
+            }
         }
     }
 
     public class EditStatesSection : AnimationPlayerUINode
     {
-        public EditStatesSection(UIRoot root, SerializedObject serializedObject, AnimationPlayerUINode parent) : base(root, serializedObject, parent) { }
+        public EditStatesSection(UIRoot root, SerializedObject serializedObject) : base(root, serializedObject) { }
 
-        public void ParentIndexChanged(int newIndex)
+        public void ParentIndexChanged(int newIndex) { }
+
+        public void OnStateAdded()
         {
+
         }
     }
 }
-
 }
